6b801e5e50435ba9d4af9007c264b12577a8ccc4
kjh3165 <jhwk109@gmail.com>
2026-05-13 11:19:52 +0900

    재귀호출을 통해서 testPostService 가 testPostRepository 를 가질 수 있도록 처리

--- src/main/java/com/domain/testPost/service/TestPostService.java
+++ src/main/java/com/domain/testPost/service/TestPostService.java
@@ -1,7 +1,11 @@
 package com.domain.testPost.service;
 
+import com.domain.testPost.repository.TestPostRepository;
 import com.framework.annotations.Service;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Service
 public class TestPostService {
+    private final TestPostRepository testPostRepository;
 }
--- src/test/java/framework/ioc/ApplicationContextTest.java
+++ src/test/java/framework/ioc/ApplicationContextTest.java
@@ -53,4 +53,17 @@ public class ApplicationContextTest {
 
         assertThat(testPostRepository).isNotNull();
     }
+
+    @Test
+    @DisplayName("testPostService has testPostRepository")
+    public void t5() {
+        TestPostService testPostService = applicationContext
+                .genBean("testPostService");
+
+        // assertThat(객체).hasFieldOrPropertyWithValue("필드명", 기대값)→ 객체 내부의 필드/프로퍼티가 해당 값과 같은지 검증.들어있는지 검증
+        assertThat(testPostService).hasFieldOrPropertyWithValue(
+                "testPostRepository",
+                applicationContext.genBean("testPostRepository")
+        );
+    }
 }
